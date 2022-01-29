a4c57c2e6dc6728495659b6a9592b7bfdf418bd6
Michael Wheatfill <michael@wheatfill.com>
2022-01-28 22:14:55 -0700

    reworked playRound function, started psuedo code for game function

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 console.log('Starting Rock, Paper, Scissors');
 // Ask player for selection
-const playerSelection = 'rock';
+const playerSelection = prompt('Type [rock, paper or scissors]');
 
+// Get random selection back for computer
 const computerPlay = () => {
   let options = ['rock', 'paper', 'scissors'];
   return options[getRandomNumber()];
@@ -11,46 +12,53 @@ const getRandomNumber = () => {
   return Math.floor(Math.random() * 3);
 };
 
-// Return random selection from computer
 const computerSelection = computerPlay();
-
-console.log(computerPlay());
 
 // Play a round with the player and computer
 const playRound = (playerSelection, computerSelection) => {
   if (playerSelection == computerSelection) {
-    return 'Its a tie.';
+    return 'Its a tie. Play again.';
   }
 
   if (playerSelection == 'rock') {
     if (computerSelection == 'paper') {
-      result = 'Computer won';
+      playerWon = false;
     } else {
-      result = 'Player won';
+      playerWon = true;
     }
   }
 
   if (playerSelection == 'paper') {
     if (computerSelection == 'scissors') {
-      result = 'Computer won';
+      playerWon = false;
     } else {
-      result = 'Player won';
+      playerWon = true;
     }
   }
 
   if (playerSelection == 'scissors') {
     if ((computerSelection = 'rock')) {
-      result = 'Computer won';
+      playerWon = false;
     } else {
-      result = 'Player won';
+      playerWon = true;
     }
   }
 
-  return result;
+  return playerWon;
+  // ? `You win! ${playerSelection} beats ${computerSelection}`
+  // : `You lost! ${computerSelection} beats ${playerSelection}`;
 };
-console.log(playRound('paper', 'scissors'));
+
+console.log(playRound(playerSelection, computerSelection));
 
 // Calculate winner. If tie, prompt again, if winner, return winner.
-// Display results to user
+// Display playerWons to user
 
 // const playerSelection = prompt('Type your selection:').toLowerCase();
+
+// game functiom that accepts one argument for how many rounds should be played
+const game = (numberOfRounds) => {
+  // object to hold game results
+  // - total rounds, rounds won by player, rounds won by computer
+  // minimum to keep score and report at the end of all rounds who won
+};

1befd9849c4f6e0212fa39b3eb338b908c8b0294
Michael Wheatfill <michael@wheatfill.com>
2022-03-03 10:53:37 -0700

    Working solution with plenty of refactoring opportunities

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 console.log('Starting Rock, Paper, Scissors');
 
-
-
 // Function to get random selection back for computer
 const computerPlay = () => {
-  let options = ['rock', 'paper', 'scissors'];
+  const options = ['rock', 'paper', 'scissors'];
   return options[getRandomNumber(3)];
 };
 
@@ -20,7 +18,7 @@ const playRound = () => {
 
   if (playerSelection == computerSelection) {
     console.log('Its a tie. Play again.');
-    return  playRound();
+    return playRound();
   }
 
   if (playerSelection == 'rock') {
@@ -46,7 +44,11 @@ const playRound = () => {
       playerWon = true;
     }
   }
-  console.log(playerWon ? `You win! ${playerSelection} beats ${computerSelection}` : `You lost! ${computerSelection} beats ${playerSelection}` )
+  console.log(
+    playerWon
+      ? `You win! ${playerSelection} beats ${computerSelection}`
+      : `You lost! ${computerSelection} beats ${playerSelection}`
+  );
   return playerWon;
   // ? `You win! ${playerSelection} beats ${computerSelection}`
   // : `You lost! ${computerSelection} beats ${playerSelection}`;
@@ -64,19 +66,27 @@ const game = (numberOfRounds) => {
     gamesPlayed: 0,
     playerScore: 0,
     computerScore: 0,
-  }
-  // - total rounds, rounds won by player, rounds won by computer
-  // minimum to keep score and report at the end of all rounds who won
-  // loop through "numberofRounds" for games
+  };
+
   for (let index = 0; index < numberOfRounds; index++) {
-    console.log(playRound());
-    gamesPlayed = index;
-
+    const round = playRound();
+    if (round) {
+      results.playerScore++;
+    } else {
+      results.computerScore++;
+    }
+    results.gamesPlayed = index + 1;
   }
-
-
-
   return results;
 };
 
-console.log(game(5));
+const gameResults = game(5);
+console.log(`Game results:`);
+console.log(`Rounds played: ${gameResults.gamesPlayed}`);
+console.log(`Your score: ${gameResults.playerScore}`);
+console.log(`Computer score: ${gameResults.computerScore}`);
+if (gameResults.playerScore > gameResults.computerScore) {
+  console.log('Congrats, you won the game!');
+} else {
+  console.log('You lost the game. Better luck next time!');
+}
